beb4fb43169e71c53f6ac0eafde9244560ae6c19
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-12-21 01:25:35 -0600

    refactor: leetcode queries

--- src/server/api/routers/leetcode.ts
+++ src/server/api/routers/leetcode.ts
@@ -2,48 +2,26 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-import { GraphQLClient } from "graphql-request";
-import type {
-  ProblemsSolvedType,
-  RecentAcceptedType,
-} from "~/util/schemas/leetcode";
 import {
-  problemsSolvedQuery,
-  recentAcceptedQuery,
+  getAcceptedProblems,
+  getProblemsSolved,
 } from "~/util/queries/graphqlLeetcode";
-
-const leetcodeEndpoint = "https://leetcode.com/graphql";
-
-const client = new GraphQLClient(leetcodeEndpoint);
 
 export const leetcodeRouter = createTRPCRouter({
   userProblems: publicProcedure
     .input(z.object({ username: z.string() }))
     .query(async ({ input }) => {
-      const parsedData = await client.request<ProblemsSolvedType>(
-        problemsSolvedQuery,
-        {
-          username: input.username,
-        },
-      );
-
-      return parsedData.matchedUser.submitStats.acSubmissionNum;
+      return await getProblemsSolved({ username: input.username });
     }),
 
   hasCompletedProblemRecently: publicProcedure
     .input(z.object({ username: z.string(), problemSlug: z.string() }))
     .query(async ({ input }) => {
-      const parsedData = await client.request<RecentAcceptedType>(
-        recentAcceptedQuery,
-        {
-          username: input.username,
-          limit: 15,
-        },
-      );
+      const recentAccepted = await getAcceptedProblems({
+        username: input.username,
+      });
 
-      console.log(parsedData);
-
-      return parsedData.recentAcSubmissionList.some(
+      return recentAccepted.some(
         (submission) => submission.titleSlug === input.problemSlug,
       );
     }),
--- src/util/queries/graphqlLeetcode.ts
+++ src/util/queries/graphqlLeetcode.ts
@@ -1,4 +1,12 @@
-import { gql } from "graphql-request";
+import { GraphQLClient, gql } from "graphql-request";
+import type {
+  ProblemsSolvedType,
+  RecentAcceptedType,
+} from "~/util/schemas/leetcode";
+
+const leetcodeEndpoint = "https://leetcode.com/graphql";
+
+export const leetcodeGraphqlClient = new GraphQLClient(leetcodeEndpoint);
 
 export const problemsSolvedQuery = gql`
   query userSessionProgress($username: String!) {
@@ -24,3 +32,30 @@ export const recentAcceptedQuery = gql`
     }
   }
 `;
+
+export const getProblemsSolved = async ({ username }: { username: string }) => {
+  const parsedData = await leetcodeGraphqlClient.request<ProblemsSolvedType>(
+    problemsSolvedQuery,
+    {
+      username: username,
+    },
+  );
+
+  return parsedData.matchedUser.submitStats.acSubmissionNum;
+};
+
+export const getAcceptedProblems = async ({
+  username,
+}: {
+  username: string;
+}) => {
+  const parsedData = await leetcodeGraphqlClient.request<RecentAcceptedType>(
+    recentAcceptedQuery,
+    {
+      username: username,
+      limit: 15,
+    },
+  );
+
+  return parsedData.recentAcSubmissionList;
+};
