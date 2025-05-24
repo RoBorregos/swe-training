a3d0174035d40e7e2a0889cf72284d0c400d460f
luiss_benvenuto <luiss.benvenuto@gmail.com>
2025-05-24 16:38:26 -0600

    Add prisma table and create problem router

---
+++ src/server/api/routers/problem.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+// import { Prisma } from "@prisma/client";
+
+export const problemRouter = createTRPCRouter({
+    getAll: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.problem.findMany({ include: { week: true, solvedBy: true } });
+    }),
+    getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
+    return ctx.db.problem.findUnique({ where: { id: input }, include: { week: true, solvedBy: true } });
+    }),
+    create: protectedProcedure
+    .input(z.object({
+        name: z.string(),
+        level: z.string(),
+        status: z.string(),
+        leetcodeUrl: z.string().url(),
+        weekId: z.string(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+        return ctx.db.problem.create({ data: input });
+    }),
+    update: protectedProcedure
+    .input(z.object({
+        id: z.string(),
+        name: z.string().optional(),
+        level: z.string().optional(),
+        status: z.string().optional(),
+        leetcodeUrl: z.string().url().optional(),
+        weekId: z.string().optional(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+        const { id, ...data } = input;
+        return ctx.db.problem.update({ where: { id }, data });
+    }),
+    delete: protectedProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
+    return ctx.db.problem.delete({ where: { id: input } });
+    }),
+});
