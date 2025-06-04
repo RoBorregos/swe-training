1ca28ca4c0b8c2281cba18bc8b7d3d6d194f4278
Héctor Tovar <h.tovarm07@gmail.com>
2025-06-04 13:29:16 -0600

    feat: added the resources page and its references to children pages

---
+++ src/app/(pages)/resources/complexities/page.tsx
@@ -0,0 +1,7 @@
+const Complexities = () => {
+    return ( 
+        <h1>Hola</h1>
+     );
+}
+ 
+export default Complexities;
--- src/app/(pages)/resources/page.tsx
+++ src/app/(pages)/resources/page.tsx
@@ -1,9 +1,31 @@
+import Link from 'next/link';
 
 const Resources = () => {
     return (
-        <div>
-            Hi
-        </div>
+        <>
+            <nav className="flex gap-10 items-center overflow-x-auto whitespace-nowrap scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 text-white">
+                <Link href="/resources/complexities">Complexities</Link>
+                <a>Arrays</a>
+                <a>Searching Algorithms</a>
+                <a>Sorting Algorithms</a>
+                <a>Hashing</a>
+                <a>Two Pointer Technique</a>
+                <a>Window Sliding Technique</a>
+                <a>Prefix Sum Technique</a>
+                <a>String</a>
+                <a>Recursion</a>
+                <a>Matrix/Grid</a>
+                <a>Linked List</a>
+                <a>Stack</a>
+                <a>Queue</a>
+                <a>Deque</a>
+                <a>Tree</a>
+                <a>Heap</a>
+                <a>Graph</a>
+                <a>Greedy Algorithm</a>
+                <a>Dynamic Programming</a>
+            </nav>
+        </>
     )
 }
 
