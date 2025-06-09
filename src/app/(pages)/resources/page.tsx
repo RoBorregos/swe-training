8ac73b398429d12eb50724ffbfca86b404e01cc0
Fernando94654 <A00841856@tec.mx>
2025-06-09 17:46:02 -0600

    added a scrollBar to the resources selector and set its global color

--- src/app/(pages)/resources/page.tsx
+++ src/app/(pages)/resources/page.tsx
@@ -4,26 +4,30 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Resources from "~/app/_components/Nav/navResources";
 import ResourceContent from "~/app/_components/resources/resourceContent";
-
+import '~/styles/globals.css'
 const HomeResources = () => {
   const [selectedResource, setSelectedResource] = useState("Resources Page");
 
   return (
-    <div className="flex">
-      <Resources onSelect={setSelectedResource} />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={selectedResource}
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -10 }}
-          transition={{ duration: 0.35 }}
-          className="p-6 flex-1 text-white relative"
-        >
-          <h1 className="text-4xl font-bold mb-4">{selectedResource}</h1>
-          <ResourceContent topic={selectedResource} />
-        </motion.div>
-      </AnimatePresence>
+    <div className="flex"> 
+      <div className="w-1/4 overflow-y-auto ">
+        <Resources onSelect={setSelectedResource} />
+      </div>
+      <div className="w-3/4 overflow-y-auto"> 
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={selectedResource}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.35 }}
+            className="p-6 flex-1 text-white relative"
+          >
+            <h1 className="text-4xl font-bold mb-4">{selectedResource}</h1>
+            <ResourceContent topic={selectedResource} />
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
