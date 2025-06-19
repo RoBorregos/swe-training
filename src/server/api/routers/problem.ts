915586eeb6f9ba455cc105bfe6bff5ce7be35bc7
Danaé <A00836760@tec.mx>
2025-06-18 23:37:33 -0600

    difficulty validation

--- src/server/api/routers/problem.ts
+++ src/server/api/routers/problem.ts
@@ -2,36 +2,48 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 // import { Prisma } from "@prisma/client";
 
+const difficultyEnum = z.enum(["EASY", "MEDIUM", "HARD"]);
+
 export const problemRouter = createTRPCRouter({
-    getAll: publicProcedure.query(async ({ ctx }) => {
+  getAll: publicProcedure.query(async ({ ctx }) => {
     return ctx.db.problem.findMany({ include: { week: true, solvedBy: true } });
-    }),
-    getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
-    return ctx.db.problem.findUnique({ where: { id: input }, include: { week: true, solvedBy: true } });
-    }),
-    create: protectedProcedure
-    .input(z.object({
+  }),
+  getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
+    return ctx.db.problem.findUnique({
+      where: { id: input },
+      include: { week: true, solvedBy: true },
+    });
+  }),
+  create: protectedProcedure
+    .input(
+      z.object({
         name: z.string(),
-        level: z.string(),
+        level: difficultyEnum, 
         leetcodeUrl: z.string().url(),
         weekId: z.string(),
-    }))
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-        return ctx.db.problem.create({ data: input });
+      return ctx.db.problem.create({ data: input });
     }),
-    update: protectedProcedure
-    .input(z.object({
+  update: protectedProcedure
+    .input(
+      z.object({
         id: z.string(),
         name: z.string().optional(),
-        level: z.string().optional(),
+        level: difficultyEnum.optional(), 
         leetcodeUrl: z.string().url().optional(),
         weekId: z.string().optional(),
-    }))
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-        const { id, ...data } = input;
-        return ctx.db.problem.update({ where: { id }, data });
+      const { id, ...data } = input;
+      return ctx.db.problem.update({ where: { id }, data });
     }),
-    delete: protectedProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
-    return ctx.db.problem.delete({ where: { id: input } });
+
+  delete: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.problem.delete({ where: { id: input } });
     }),
 });
