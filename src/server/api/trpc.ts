ea34e8da3d160305990cc73ca3ffdf5f8ea0efac
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-06-18 20:57:37 -0600

    feat: add user roles

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -132,3 +132,24 @@ export const protectedProcedure = t.procedure
       },
     });
   });
+
+export const memberProcedure = protectedProcedure.use(async ({ ctx, next }) => {
+  if (ctx.session.user.role === "USER") {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({ ctx });
+});
+
+/**
+ * Admin procedure
+ *
+ * Only for admins registered in the database
+ */
+export const adminProcedure = memberProcedure.use(async ({ ctx, next }) => {
+  if (ctx.session.user.role !== "ADMIN") {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({ ctx });
+});
